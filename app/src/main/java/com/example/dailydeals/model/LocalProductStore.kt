a4cd1c410c88b3a599e6fca73a9fcef2ffb69b63
QasimzadaEA <123947841+ElgunGasimzade@users.noreply.github.com>
2026-03-02 18:17:00 +0400

    package base change and default loca changed to 1km

--- app/src/main/java/com/example/dailydeals/model/LocalProductStore.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dailydeals.model
-
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-
-object LocalProductStore {
-    var selected: Product? by mutableStateOf(null)
-}
